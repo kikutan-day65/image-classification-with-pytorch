a908cd7e01d141784d405e3555522ccb08fac802
Haruto Mori <baystarsman@outlook.jp>
2023-10-29 23:13:22 +0900

    split datasets into train_data, test_data and val_data

--- exercise12_mori.py
+++ exercise12_mori.py
@@ -2,8 +2,7 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
-import matplotlib.pyplot as plt
-
+from torch.utils.data import random_split, DataLoader
 
 data_dir = './datasets'
 
@@ -13,3 +12,18 @@ transform = transforms.Compose([
 ])
 
 dataset = ImageFolder(root=data_dir, transform=transform)
+
+dataset_size = len(dataset)
+
+train_ratio = 0.8
+test_ratio = 0.2
+
+train_size = int(train_ratio * dataset_size)
+test_size = int(test_ratio * dataset_size)
+val_size = dataset_size - train_size - test_size
+
+train_dataset, test_dataset, val_dataset = random_split(dataset, [train_size, test_size, val_size])
+
+print(f"トレーニングセットのサイズ: {len(train_dataset)}")
+print(f"テストセットのサイズ: {len(test_dataset)}")
+print(f"バリデーションセットのサイズ: {len(val_dataset)}")
