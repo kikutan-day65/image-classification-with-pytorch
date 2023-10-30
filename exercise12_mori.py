ccd9d8da477625d6ba04c9a3094efed727d102d7
Haruto Mori <baystarsman@outlook.jp>
2023-10-30 11:03:48 +0900

    set the batch size and split datas into train and val

--- exercise12_mori.py
+++ exercise12_mori.py
@@ -2,7 +2,8 @@ import torch
 import torchvision
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
-import matplotlib.pyplot as plt
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data import random_split
 
 
 data_dir = './datasets'
@@ -13,3 +14,18 @@ transform = transforms.Compose([
 ])
 
 dataset = ImageFolder(data_dir, transform=transform)
+
+batch_size = 64
+
+dataset_size = len(dataset)
+
+train_ratio = 0.8
+val_ratio = 0.2
+
+train_size = int(train_ratio * dataset_size)
+val_size = dataset_size - train_size
+
+train_data, val_data = random_split(dataset, [train_size, val_size])
+
+train_dl = DataLoader(train_data, batch_size, shuffle=True, num_workers=4, pin_memory=True)
+val_dl = DataLoader(val_data, batch_size*2, num_workers=4, pin_memory=True)
