12cf23e279e195a2985b8e9cbb28e66fc6f9cc92
Haruto Mori <baystarsman@outlook.jp>
2023-10-30 10:25:26 +0900

    preprocessing of datasets

--- exercise12_mori.py
+++ exercise12_mori.py
@@ -2,4 +2,14 @@ import torch
 import torchvision
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
+import matplotlib.pyplot as plt
 
+
+data_dir = './datasets'
+
+transform = transforms.Compose([
+    transforms.Resize((150, 150)),
+    transforms.ToTensor()
+])
+
+dataset = ImageFolder(data_dir, transform=transform)
