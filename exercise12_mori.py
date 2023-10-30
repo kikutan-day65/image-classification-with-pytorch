ef9f43b1f7680d5df905bead6ac1bf3bbba97d97
Haruto Mori <baystarsman@outlook.jp>
2023-10-30 10:16:50 +0900

    import libs to set datasets

--- exercise12_mori.py
+++ exercise12_mori.py
@@ -1,47 +1,5 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torchvision.transforms as transforms
+import torchvision
+from torchvision import transforms
 from torchvision.datasets import ImageFolder
-from torch.utils.data import random_split, DataLoader
-from models import ImageNetCNN
 
-
-data_dir = './datasets'
-
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
-])
-
-dataset = ImageFolder(root=data_dir, transform=transform)
-
-dataset_size = len(dataset)
-
-train_ratio = 0.8
-test_ratio = 0.2
-
-train_size = int(train_ratio * dataset_size)
-test_size = int(test_ratio * dataset_size)
-val_size = dataset_size - train_size - test_size
-
-train_dataset, test_dataset, val_dataset = random_split(dataset, [train_size, test_size, val_size])
-
-batch_size = 32
-
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size)
-val_loader = DataLoader(val_dataset, batch_size=batch_size)
-
-# training
-learning_rate = 0.001
-num_epochs = 10
-
-num_classes = 3
-model = ImageNetCNN(num_classes)
-
-# loss function
-criterion = nn.CrossEntropyLoss()
-
-# optimization algorithm
-optimizer = optim.SGD(model.parameters(), lr=learning_rate)
