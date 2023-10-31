421431c1faaefcda12c95a71e9854489e745e3c2
Haruto Mori <baystarsman@outlook.jp>
2023-10-31 11:21:54 +0900

    modify the cnn model better

--- cnnModel.py
+++ cnnModel.py
@@ -1,27 +1,23 @@
 import torch.nn as nn
+import torch.nn.functional as F
+import torch
 
 
 class CNNImageClassifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.network = nn.Sequential(
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
-            nn.Conv2d(
-                in_channels=3, out_channels=32,
-                kernel_size=3, stride=1, padding=1
-            ),
-            nn.ReLU(),
-            nn.Conv2d(
-                in_channels=32, out_channels=64,
-                kernel_size=3, stride=1, padding=1
-            ),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Flatten(),
-            nn.Linear(in_features=64*37*37, out_features=512, bias=True),
-            nn.ReLU(),
-            nn.Linear(in_features=512, out_features=6, bias=True)
-        )
-    
     def forward(self, x):
-        return self.network(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
