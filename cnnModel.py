141d14ae9593f7c7a2a963e3cbbd3e48890b6195
Haruto Mori <baystarsman@outlook.jp>
2023-10-31 10:10:05 +0900

    modify the cnn model

--- cnnModel.py
+++ cnnModel.py
@@ -1,37 +1,27 @@
 import torch.nn as nn
-from baseModel import ImageClassificationBase
 
 
-class NaturalSceneClassification(ImageClassificationBase):
+class CNNImageClassifier(nn.Module):
     def __init__(self):
         super().__init__()
         self.network = nn.Sequential(
-            
-            nn.Conv2d(3, 32, kernel_size = 3, padding = 1),
+
+            nn.Conv2d(
+                in_channels=3, out_channels=32,
+                kernel_size=3, stride=1, padding=1
+            ),
             nn.ReLU(),
-            nn.Conv2d(32,64, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv2d(
+                in_channels=32, out_channels=64,
+                kernel_size=3, stride=1, padding=1
+            ),
             nn.ReLU(),
-            nn.MaxPool2d(2,2),
-        
-            nn.Conv2d(64, 128, kernel_size = 3, stride = 1, padding = 1),
-            nn.ReLU(),
-            nn.Conv2d(128 ,128, kernel_size = 3, stride = 1, padding = 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2,2),
-            
-            nn.Conv2d(128, 256, kernel_size = 3, stride = 1, padding = 1),
-            nn.ReLU(),
-            nn.Conv2d(256,256, kernel_size = 3, stride = 1, padding = 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2,2),
-            
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Flatten(),
-            nn.Linear(82944,1024),
+            nn.Linear(in_features=64*37*37, out_features=512, bias=True),
             nn.ReLU(),
-            nn.Linear(1024, 512),
-            nn.ReLU(),
-            nn.Linear(512,6)
+            nn.Linear(in_features=512, out_features=6, bias=True)
         )
     
-    def forward(self, xb):
-        return self.network(xb)
+    def forward(self, x):
+        return self.network(x)
